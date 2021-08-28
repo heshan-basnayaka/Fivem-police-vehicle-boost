ea46e9493d027857779282379d9203ca1f35d425
Heshan Basnayaka <34080199+3UR3K4@users.noreply.github.com>
2021-08-28 18:21:32 +0530

    Update __resource.lua

--- __resource.lua
+++ __resource.lua
@@ -1,5 +1,5 @@
 resource_manifest_version '44febabe-d386-4d18-afbe-5e627f4af937'
 
-client_script 'client.lua'
+client_script 'speed_client.lua'
 
 description 'police Vehicle speed boost By : 3UR3K4'
